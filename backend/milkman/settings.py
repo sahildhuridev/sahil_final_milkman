307216d08fbe089e0ce428c0f724e84f889eff34
sahildhuridev <sahildhuri.dev216@gmail.com>
2026-03-09 02:13:29 +0530

    added frontend vercel url

--- backend/milkman/settings.py
+++ backend/milkman/settings.py
@@ -148,6 +148,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://sahil-final-milkman.vercel.app",
 ]
 
 FRONTEND_BASE_URL = config('FRONTEND_BASE_URL', default='http://localhost:5173')
